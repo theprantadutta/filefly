b16e4110fa2bef6b5e6ca5a59d2b05931f4a8263
Pranta Dutta <prantadutta1997@gmail.com>
2024-02-06 22:02:13 +0600

    added delete function

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use std::env;
 use std::time::Instant;
 
+use copy_files_with_progress::delete_folder_with_progress;
+
 use crate::copy_files_with_progress::copy_files_with_progress;
 use crate::logger::Logger;
 
 mod copy_files_with_progress;
 mod logger;
+
 
 fn main() {
     // Record the start time
@@ -15,10 +18,12 @@ fn main() {
     if let Ok(current_dir) = env::current_dir() {
         // Assuming your project root is the directory containing the Cargo.toml file
         let copy_from = current_dir.join("temp");
-        let copy_to = current_dir.join("output");
-
+        
+        // let copy_to = current_dir.join("output");
+        let copy_to = current_dir.join(r"C:\Users\prant\Downloads\Compressed");
         Logger.debug(&format!("Copying All Files From {} To {}", copy_from.to_str().unwrap(), copy_to.to_str().unwrap()));
-        let result = copy_files_with_progress(&copy_from, &copy_to);
+        // let result = copy_files_with_progress(&copy_from, &copy_to);
+        let result = delete_folder_with_progress(copy_to);
 
         match result {
             Ok(_) => Logger.success("Copying Successful"),
