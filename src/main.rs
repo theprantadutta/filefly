768db74f5cf66b43d70976dfe39a1b3800e9a900
PRANTA Dutta <pranta.dutta@kdsgroup.net>
2024-10-10 16:22:06 +0600

    just formatted

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use clap::Parser;
 use filefly_args::FileFlyArgs;
 use handlers::{
-    handle_copy_command::handle_copy_command, 
-    handle_delete_command::handle_delete_command,
+    handle_copy_command::handle_copy_command, handle_delete_command::handle_delete_command,
     handle_replace_command::handle_replace_command,
-    handle_synchronize_command::handle_synchronize_command
+    handle_synchronize_command::handle_synchronize_command,
 };
 use logger::Logger;
 
