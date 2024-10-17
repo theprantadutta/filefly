f0b084c83bac026c80dfff4d90c0b820040c0838
Pranta Dutta <prantadutta1997@gmail.com>
2024-10-17 23:53:30 +0600

    updated install script

--- install.ps1
+++ install.ps1
@@ -2,12 +2,29 @@
 $repoOwner = "theprantadutta"
 $repoName = "filefly"
 
+# Headers for GitHub API
+$headers = @{
+    "User-Agent" = "Mozilla/5.0"
+}
+
 # Get the latest release version from GitHub API
-$latestRelease = Invoke-RestMethod -Uri "https://api.github.com/repos/$repoOwner/$repoName/releases/latest"
+$latestRelease = Invoke-RestMethod -Uri "https://api.github.com/repos/$repoOwner/$repoName/releases/latest" -Headers $headers
 $version = $latestRelease.tag_name
+
+# Check if assets exist
+if ($latestRelease.assets -eq $null) {
+    Write-Host "No assets found in the latest release."
+    exit
+}
 
 # Find the download URL for the Windows version
 $windowsAssetUrl = $latestRelease.assets | Where-Object { $_.name -like "*windows.exe" } | Select-Object -ExpandProperty browser_download_url
+
+# Check if the Windows asset URL is found
+if (-Not $windowsAssetUrl) {
+    Write-Host "No Windows executable found in the release assets."
+    exit
+}
 
 # Define installation path in the user's directory
 $installDir = "$HOME\AppData\Local\Programs\filefly"
@@ -20,14 +37,20 @@ if (-Not (Test-Path $installDir)) {
 # Download the binary for Windows
 $exePath = "$installDir\filefly_v$version.exe"
 Write-Host "Downloading Filefly version $version for Windows..."
-Invoke-WebRequest -Uri $windowsAssetUrl -OutFile $exePath
+try {
+    Invoke-WebRequest -Uri $windowsAssetUrl -OutFile $exePath -ErrorAction Stop
+} catch {
+    Write-Host "Error downloading the executable: $_"
+    exit
+}
 
 # Rename the downloaded file to "filefly.exe" for consistency
 $finalExePath = "$installDir\filefly.exe"
-if (-Not (Test-Path $finalExePath)) {
+if (Test-Path $exePath) {
     Rename-Item -Path $exePath -NewName $finalExePath
 } else {
-    Write-Host "filefly.exe already exists in $installDir. Skipping rename."
+    Write-Host "Downloaded file not found. Something went wrong."
+    exit
 }
 
 # Add to user's PATH permanently
